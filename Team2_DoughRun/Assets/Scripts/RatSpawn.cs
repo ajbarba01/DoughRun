cf6cad753d0df61da14c0fd13909b0434bbde734
wilkinsonJay <72212972+wilkinsonJay@users.noreply.github.com>
2025-03-02 17:00:14 -0500

    Created Sewage drain spawn point for rats

---
+++ Team2_DoughRun/Assets/Scripts/RatSpawn.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatSpawn : MonoBehaviour
+{
+
+    public GameObject RatEnemy;
+    public Transform drainLocation;
+    private bool canSpawn = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerStay2D(Collider2D other) {
+        if (other.CompareTag("Player"))
+        {
+            if (canSpawn) {
+                Debug.Log("Spawning rat");
+                Instantiate(RatEnemy, drainLocation);
+                StartCoroutine(SpawnCooldown());
+            }
+        }
+    }
+
+    private IEnumerator SpawnCooldown()
+    {
+        canSpawn = false;
+        yield return new WaitForSeconds(10f);
+        canSpawn = true;
+    }
+}
