fa9f0ca6f8912f2ce50dbce7a792f3b0f0639889
ajbarba01 <Alexander.Barba@tufts.edu>
2025-03-03 21:24:12 -0500

    Controls Menu

--- Team2_DoughRun/Assets/Scripts/EndTitle.cs
+++ Team2_DoughRun/Assets/Scripts/EndTitle.cs
@@ -10,7 +10,6 @@ public class EndTitle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("UHHH");
         if (GameHandler.won) {
             endTitle.text = "YOU WON!";
         }
